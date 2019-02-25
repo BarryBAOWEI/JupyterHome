43b80bf100e7fee5b7dfbd8c6a84ac8820688c36
BarryLu <434609683@qq.com>
2019-02-25 18:34:50 +0800

    PaperData DownloadUpDate

--- paper_R/garch_learning/szzs_garch_learning.R
+++ paper_R/garch_learning/szzs_garch_learning.R
@@ -3,14 +3,15 @@ library(fUnitRoots)
 library(xts)
 library(FinTS)
 library(fitdistrplus)
+library(fGarch)
 
 # 安装包 or 从github上安装包
-# install.packages('devtools')
+# install.packages('fGarch')
 # library(devtools)
 # install_github("cran/FinTS")
 # library(FinTS)
 
-data<-read.zoo('C:/Users/jxjsj/Desktop/JupyterHome/Data/SZZS_lnr_rv_w_m_ntd_080101-181101.csv',header=TRUE,sep=',')
+data <- read.zoo('C:/Users/jxjsj/Desktop/JupyterHome/Data/SZZS_lnr_rv_w_m_ntd_080101-181101.csv',header=TRUE,sep=',')
 sample <- xts(x = data)
 
 # 上证指数2008-01-03 - 2018-11-01日历效应
@@ -20,12 +21,133 @@ sample <- xts(x = data)
 ## 2014.11初-2016.03末[1659:2004],
 ## 2016.02初-2018.11初*[1966:2635].
 sample.test = sample[489:1701,]
+sample.all = sample[489:2635,]
+
+# 回归准备-模型设定
+
+## 对数收益率序列的弱平稳性检验
+adfTest(sample.all[,1], lags = 10, type =c("c"), title = NULL, description =NULL) # type =c("nc", "c", "ct")
+
+## 对数收益率的Ljung-Box统计量 - 检验是否需要进行GARCH建模
+m <- round(log(length(sample.all[,1]))) # Q(m)=ln(T)
+lnR.mean <- mean(sample.all[,1])
+at <- sample.all[,1]-lnR.mean
+Box.test(at,lag=m, type='Ljung')        # Ljung-Box统计量,对数收益率的自相关,不显著则收益率不存在序列相关（仅存在弱序列相关）
+Box.test(at^2,lag=m, type='Ljung')      # Ljung-Box统计量,对数收益率平方的自相关,显著则则收益率序列不独立,均满足则适合GARCH建模
+
+###########################################################################################
+## 分布选择
+
+### (1)正态分布-norm
+spec.mod1 = ugarchspec(
+  variance.model = list(model = "realGARCH", garchOrder = c(1,1)), 
+  mean.model = list(armaOrder = c(0,0), include.mean = TRUE), 
+  distribution.model = "norm")
+sgarch.mod1 = ugarchfit(data=sample.all[,1], spec = spec.mod1, 
+                        solver = "hybrid", realizedVol = sample.all[,3])
+
+#### 回归结果
+sgarch.mod1
+
+#### 标准化残差的Ljung-Box统计量 - 检验建模是否充分
+m <- round(log(length(sample.all[,1])))                # Q(m)=ln(T)
+garch_at.mod1 = sgarch.mod1@fit[["residuals"]]
+garch_sigma.mod1 = sgarch.mod1@fit[["sigma"]]
+stdd_residuals.mod1 = garch_at.mod1/garch_sigma.mod1
+Box.test(stdd_residuals.mod1,lag=m, type='Ljung')      # Ljung-Box统计量,标准残差的自相关,不显著则收益率方程建模充分
+Box.test(stdd_residuals.mod1^2,lag=m, type='Ljung')    # Ljung-Box统计量,标准残差平方的自相关,不显著则波动率方程建模充分
+
+#### QQ图
+plot(sgarch.mod1)
+norm.test <- rnorm(10000, 0, 1)
+ks.test(stdd_residuals.mod1,norm.test)
+
+#### 已实现波动与预测波动的回归分析比较：RV = a + b*sigma + u，比较R^2 & |b-1| ！不太好用！
+garch_sigma.mod1 = sgarch.mod1@fit[["sigma"]]^2
+RV_sigma.mod1 = data.frame(y=as.vector(sample.all[,2]),
+                           var1=c(garch_sigma.mod1))
+lm_RV_sigma_test.mod1 = lm(y~1+var1,data=RV_sigma.mod1)
+##### R-square 越大越好
+summary(lm_RV_sigma_test.mod1)$r.squared
+##### |b-1| 越小越好
+abs(summary(lm_RV_sigma_test.mod1)$coeff[2]-1)
+
+
+### (2)学生分布-std
+spec.mod2 = ugarchspec(
+  variance.model = list(model = "realGARCH", garchOrder = c(1,1)), 
+  mean.model = list(armaOrder = c(0,0), include.mean = TRUE), 
+  distribution.model = "std")
+sgarch.mod2 = ugarchfit(data=sample.all[,1], spec = spec.mod2, 
+                        solver = "hybrid", realizedVol = sample.all[,3])
+
+#### 回归结果
+sgarch.mod2
+
+#### 标准化残差的Ljung-Box统计量 - 检验建模是否充分
+m <- round(log(length(sample.all[,1])))                # Q(m)=ln(T)
+garch_at.mod2 = sgarch.mod2@fit[["residuals"]]
+garch_sigma.mod2 = sgarch.mod2@fit[["sigma"]]
+stdd_residuals.mod2 = garch_at.mod2/garch_sigma.mod2
+Box.test(stdd_residuals.mod2,lag=m, type='Ljung')      # Ljung-Box统计量,标准残差的自相关,不显著则收益率方程建模充分
+Box.test(stdd_residuals.mod2^2,lag=m, type='Ljung')    # Ljung-Box统计量,标准残差平方的自相关,不显著则波动率方程建模充分
+
+#### QQ图
+plot(sgarch.mod2)
+std.test <- rt(10000, 4.5)
+ks.test(stdd_residuals.mod2,std.test)
+
+#### 已实现波动与预测波动的回归分析比较：RV = a + b*sigma + u，比较R^2 & |b-1|
+garch_sigma.mod2 = sgarch.mod2@fit[["sigma"]]^2
+RV_sigma.mod2 = data.frame(y=as.vector(sample.all[,2]),
+                           var1=c(garch_sigma.mod2))
+lm_RV_sigma_test.mod2 = lm(y~1+var1,data=RV_sigma.mod2)
+##### R-square 越大越好
+summary(lm_RV_sigma_test.mod2)$r.squared
+##### |b-1| 越小越好
+abs(summary(lm_RV_sigma_test.mod2)$coeff[2]-1)
+
+
+### (3)广义误差分布-ged
+spec.mod3 = ugarchspec(
+  variance.model = list(model = "realGARCH", garchOrder = c(1,1)), 
+  mean.model = list(armaOrder = c(0,0), include.mean = TRUE), 
+  distribution.model = "ged")
+sgarch.mod3 = ugarchfit(data=sample.all[,1], spec = spec.mod3, 
+                        solver = "hybrid", realizedVol = sample.all[,3])
+
+#### 回归结果
+sgarch.mod3
+
+#### 标准化残差的Ljung-Box统计量 - 检验建模是否充分
+m <- round(log(length(sample.all[,1])))                # Q(m)=ln(T)
+garch_at.mod3 = sgarch.mod3@fit[["residuals"]]
+garch_sigma.mod3 = sgarch.mod3@fit[["sigma"]]
+stdd_residuals.mod3 = garch_at.mod3/garch_sigma.mod3
+Box.test(stdd_residuals.mod3,lag=m, type='Ljung')      # Ljung-Box统计量,标准残差的自相关,不显著则收益率方程建模充分
+Box.test(stdd_residuals.mod3^2,lag=m, type='Ljung')    # Ljung-Box统计量,标准残差平方的自相关,不显著则波动率方程建模充分
+
+#### QQ图
+plot(sgarch.mod3)
+ged.test <- rged(10000, 0, 1 ,1.2)
+ks.test(stdd_residuals.mod3,ged.test)
+
+#### 已实现波动与预测波动的回归分析比较：RV = a + b*sigma + u，比较R^2 & |b-1|
+garch_sigma.mod3 = sgarch.mod3@fit[["sigma"]]^2
+RV_sigma.mod3 = data.frame(y=as.vector(sample.all[,2]),
+                           var1=c(garch_sigma.mod3))
+lm_RV_sigma_test.mod3 = lm(y~1+var1,data=RV_sigma.mod3)
+##### R-square
+summary(lm_RV_sigma_test.mod3)$r.squared
+##### |b-1|
+abs(summary(lm_RV_sigma_test.mod3)$coeff[2]-1)
+###########################################################################################
 
 ### 周内效应总体检验
 
 #### 周内效应-norm
 spec1 = ugarchspec(
-  variance.model = list(model = "realGARCH", garchOrder = c(2,1)), 
+  variance.model = list(model = "realGARCH", garchOrder = c(1,1)), 
   mean.model = list(armaOrder = c(0,0), include.mean = TRUE, external.regressors = sample.test[,c(16,17,19,20)]), 
   distribution.model = "norm")
 sgarch_test1 = ugarchfit(data=sample.test[,1], spec = spec1, 
@@ -35,7 +157,7 @@ plot(sgarch_test1)
 
 #### 周内效应-ged
 spec2 = ugarchspec(
-  variance.model = list(model = "realGARCH", garchOrder = c(2,1)), 
+  variance.model = list(model = "realGARCH", garchOrder = c(1,1)), 
   mean.model = list(armaOrder = c(0,0), include.mean = TRUE, external.regressors = sample.test[,c(16,17,19,20)]), 
   distribution.model = "ged")
 sgarch_test2 = ugarchfit(data=sample.test[,1], spec = spec2, 
@@ -191,7 +313,7 @@ plot(sgarch_test15)
 
 ########################各类检验##########################
 #  对数收益率序列的弱平稳性检验
-adfTest(sample.test[,1], lags = 1, type =c("c"), title = NULL, description =NULL) # type =c("nc", "c", "ct")
+adfTest(sample.test[,1], lags = 10, type =c("c"), title = NULL, description =NULL) # type =c("nc", "c", "ct")
 
 # 寻找对数收益率序列最合适的分布-1
 # # 正态分布
@@ -203,7 +325,7 @@ adfTest(sample.test[,1], lags = 1, type =c("c"), title = NULL, description =NULL
 
 # 寻找对数收益率序列最合适的分布-2
 # 已实现波动与预测波动的回归分析比较
-garch_sigma = sgarch_test4@fit[["sigma"]]^2
+garch_sigma = sgarch_test1@fit[["sigma"]]^2
 RV_sigma = data.frame(y=as.vector(sample.test[,2]),
                       var1=c(garch_sigma))
 lm.RV_sigma_test = lm(y~1+var1,data=RV_sigma)
@@ -220,12 +342,12 @@ Box.test((sample.test[,1]-mean(sample.test[,1]))^2,lag=12, type='Ljung')
 Box.test(sample.test[,1],lag=3, type='Ljung')
 
 # 标准化残差的arch效应检验LM统计量与Ljung-Box统计量
-garch_at3 = sgarch_test3@fit[["residuals"]]
-garch_sigma3 = sgarch_test3@fit[["sigma"]]
-stdd.residuals3 = garch_at3/garch_sigma3
-ArchTest(stdd.residuals3,lag=7)                    # LM统计量
-Box.test(stdd.residuals3,lag=7, type='Ljung')      # Ljung-Box统计量,标准残差的自相关,收益率方程建模充分
-Box.test(stdd.residuals3^2,lag=7, type='Ljung')    # Ljung-Box统计量,标准残差平方的自相关,波动率方程建模充分
+garch_at1 = sgarch_test1@fit[["residuals"]]
+garch_sigma1 = sgarch_test1@fit[["sigma"]]
+stdd.residuals1 = garch_at1/garch_sigma1
+ArchTest(stdd.residuals1,lag=7)                    # LM统计量
+Box.test(stdd.residuals1,lag=7, type='Ljung')      # Ljung-Box统计量,标准残差的自相关,收益率方程建模充分
+Box.test(stdd.residuals1^2,lag=7, type='Ljung')    # Ljung-Box统计量,标准残差平方的自相关,波动率方程建模充分
 
 
 ########################乱七八糟##########################
